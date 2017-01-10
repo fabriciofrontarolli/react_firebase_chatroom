a9e10014ebd3f17cf9ee226accba3e9321a83a65
Fabricio Frontarolli <fabricio.frontarolli@gmail.com>
2017-01-10 02:12:21 +0000

    A simple React Application Chatroom using Firebase as Data Source

---
+++ src/components/ChatRoom.js
@@ -0,0 +1,80 @@
+import React, {Component} from 'react';
+
+class ChatRoom extends Component {
+
+    constructor(props, context){
+        super(props, context);
+
+        this.handleChatMessage = this.handleChatMessage.bind(this);
+        this.handleUpdateMessage = this.handleUpdateMessage.bind(this);
+
+        this.state = {
+            message: '',
+            messages: []
+        };
+    }
+
+    handleUpdateMessage(e){
+        this.setState({
+            message: e.target.value
+        });
+    }
+
+    handleChatMessage(e){
+        const message = this.state.message;
+        const newMessage = {
+            id: this.state.messages.length,
+            message
+        }
+
+        /* send to firebase */
+        firebase.database()
+                .ref(`messages/${newMessage.id}`)
+                .set(newMessage);
+        // const newMessageList = Object.assign([], this.state.messages);
+        // newMessageList.push(newMessage);
+        //
+        // this.setState({
+        //     messages: newMessageList
+        // });
+    }
+
+    componentDidMount(){
+        firebase.database()
+                .ref('messages/')
+                .on('value', (snapshot) => {
+
+                    const currentMessages = snapshot.val()
+
+                    console.log('currentMessages');
+                    console.log(currentMessages);
+
+                    if(currentMessages != null){
+                        this.setState({
+                            messages: currentMessages
+                        })
+                    }
+                });
+    }
+
+    render(){
+        const chatMessages = this.state.messages.map((m) => {
+                                return <li key={m.id}>{m.message}</li>
+                             });
+
+        return(
+            <div>
+                <div id="chat-output">
+                    { chatMessages }
+                </div>
+                <div id="chat-input">
+                    <label>Send your message</label>
+                    <input type="text" onChange={this.handleUpdateMessage} />
+                    <button onClick={this.handleChatMessage}>submit message</button>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default ChatRoom;
